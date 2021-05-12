01813784482439bc713dfd5a4f85d23a3738f5c7
Isi Tony-Eigbokhan <itonyeig@gmail.com>
2021-05-12 19:31:12 +0100

    socket-io-boilerplate

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+const chatForm = document.getElementById('chat-form')
+
+var socket = io();// do this to bring io
+
+//when the user types in a message, the server gets it as a chat message event.
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const msg = e.target.elements.msg.value
+  console.log(msg);
+})
+//this is what connects us to the backend
+socket.on('message', (message) => {
+  console.log(message);
+})
+
---
+++ server.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);//// initializes app to be a function handler that you can supply to an HTTP server
+
+// Integrating Socket.IO
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Listen for connection event for incoming sockets and log it to the console
+io.on('connection', (socket) => {
+  console.log('new webservice connection');
+
+  //broadcasts only to the client that just connected
+  socket.emit('message', 'link is established on the front end');
+  //broadcast to clients already connected that a new client is connected. New client does not get this broadcast
+  socket.broadcast.emit('message', 'a user has joined the chat');
+
+  // this runs when a client disconnects
+  socket.on
+
+  //send message. broadcast to all clients connected.
+  // io.emit()
+
+  //disconnect
+});
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+//  my server now listents to connections on the PORT connection
+server.listen(PORT, () => {
+  console.log(`***Server is running on ${PORT}***`);
+})

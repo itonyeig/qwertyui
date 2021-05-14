2d611d867dd7f4342c62892abb6ae2a6bb099e11
Isi Tony-Eigbokhan <itonyeig@gmail.com>
2021-05-14 09:25:33 +0100

    basic chat

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,50 @@
 const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
 
-var socket = io();// do this to bring io
+var socket = io();// do this to bring io 
 
-//when the user types in a message, the server gets it as a chat message event.
-chatForm.addEventListener('submit', (e) => {
-  e.preventDefault();
 
-  const msg = e.target.elements.msg.value
-  console.log(msg);
-})
-// this is what connects us to the backend
-socket.on('message', (message) => {
-  console.log(message);
-})
+
+
+
+// // this is what connects us to the backend
+// socket.on('message', (message) => {
+// //console.log(message);
+//   outPutMessage(message)
+
+//   // scroll down
+//   chatMessages.scrollTop = chatMessages.scrollHeight// chat scroll
+//   window.scrollTo(0, document.body.scrollHeight);//window scroll
+
+// })
+
+// //when the user/client types in a message, the server gets it as a chat message event.
+// chatForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   //get message test
+//   const msg = e.target.elements.msg.value
+  
+//   //send the message the client/user typed in to the server (sends this message to all servers that are connected)
+//   socket.emit('chatMessage', msg);
+  
+//   //clear input
+//   e.target.elements.msg.value = ''
+//   //focus on input
+//   e.target.elements.msg.focus()
+// })
+
+// //DOM manipulation is done here so we can ouptut the messaage that the user/client keyed in
+// function outPutMessage(message) {
+//   const div = document.createElement('div')
+//   const cls = ["container", "darker"];
+//   div.classList.add(...cls)//class from html
+//   div.innerHTML = `<img src=${message.imageTestLightUser} alt="Avatar" class="right">
+//         <p>${message.text}</p>
+//         <span class="time-left">${message.time}</span>`
+//   //put it into the DOM
+//   document.querySelector('.chat-messages').appendChild(div) 
+  
+
+// }
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express');
+const formatMessage = require('./utils/messages')
 const app = express();
 const http = require('http');
 const server = http.createServer(app);//// initializes app to be a function handler that you can supply to an HTTP server
@@ -17,23 +18,30 @@ io.on('connection', (socket) => {
   console.log('new webservice connection');
 
   //broadcasts only to the client that just connected
-  socket.emit('message', 'link is established on the front end');
+  // socket.emit('message', 'link is established on the front end');
+  
   //broadcast to clients already connected that a new client is connected. New client does not get this broadcast
   socket.broadcast.emit('message', 'a user has joined the chat');
 
   // this runs when a client disconnects
-  socket.on
+  
 
   //send message. broadcast to all clients connected.
   // io.emit()
 
   //disconnect
+
+  //listen for 'chatMessage' from client/user
+  socket.on('chatMessage', (msg) => {
+    //emitts it to all users
+    io.emit('message', formatMessage('USER', msg))
+  })
 });
 
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 
 //  my server now listents to connections on the PORT connection
 server.listen(PORT, () => {
